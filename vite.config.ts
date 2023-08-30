2a8323df258aaf096de7b378d35e4ffebf460228
微光北下 <wgbx@foxmail.com>
2023-08-30 17:51:25 +0800

    feat: 构建vite编译代码

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import libCss from 'vite-plugin-libcss'
+
+export default defineConfig({
+  build: {
+    cssCodeSplit: true,
+    watch: {
+      include: 'src/**'
+    },
+    lib: {
+      entry: resolve('packages', 'index.ts'),
+      name: 'VueViewLoading',
+      fileName: 'vue3-view-loading'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: [vue(), dts(), libCss()]
+})
